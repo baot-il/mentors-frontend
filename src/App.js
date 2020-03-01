8741ffc18c355521ffc3a908e3d3ece4d01323aa
dafnarosenblum <dafi.moin@gmail.com>
2020-03-01 12:42:33 +0100

    Add Login component with Firebase auth

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import React from "react";
 import "./styles.css";
 import Checkout from "./Checkout";
-import { create } from 'jss';
-import rtl from 'jss-rtl';
-import { ThemeProvider, StylesProvider, jssPreset } from '@material-ui/core/styles';
-import { theme } from './MuiTheme'
+import Login from "./Login";
+import { create } from "jss";
+import rtl from "jss-rtl";
+import {
+  ThemeProvider,
+  StylesProvider,
+  jssPreset
+} from "@material-ui/core/styles";
+import { theme } from "./MuiTheme";
+import * as firebase from "firebase/app";
 
 // Configure JSS
-const jss = create({ plugins: [...jssPreset().plugins, rtl()] });;
+const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
-    return (<ThemeProvider theme={theme}>
-        <StylesProvider jss={jss}>
-            <div className="App" dir='rtl'>
-                <Checkout/>
-            </div
-        ></StylesProvider>
-    </ThemeProvider>);
+  return (
+    <ThemeProvider theme={theme}>
+      <StylesProvider jss={jss}>
+        <div className="App" dir="rtl">
+          <Login></Login>
+          {/* <Checkout /> */}
+        </div>
+      </StylesProvider>
+    </ThemeProvider>
+  );
 }
