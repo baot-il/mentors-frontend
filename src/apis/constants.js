dfffd003ab5d6f969e32527d90f45085c62b6509
dafnarosenblum <dafi.moin@gmail.com>
2020-03-01 19:16:47 +0100

    Add react router and redirect after login

--- src/apis/constants.js
+++ src/apis/constants.js
@@ -1,4 +1,5 @@
-export const PRODUCTION = 'https://baot-mentors-prod.herokuapp.com';
-export const STAGING = 'https://baot-mentors-stage.herokuapp.com';
+export const PRODUCTION = "https://baot-mentors-prod.herokuapp.com";
+export const STAGING = "https://baot-mentors-stage.herokuapp.com";
+const LOCALHOST = "localhost:5000";
 
 export const ENV = STAGING;
