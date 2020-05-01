63f7c78d982cdeeefba07f940531dcc5e0debf59
dafnarosenblum <dafi.moin@gmail.com>
2020-05-01 10:40:23 +0200

    Fix env var for server name

--- src/apis/constants.js
+++
@@ -1,5 +0,0 @@
-export const PRODUCTION = "https://baot-mentors-prod.herokuapp.com";
-export const STAGING = "https://baot-mentors-stage.herokuapp.com";
-const LOCALHOST = "localhost:5000";
-
-export const ENV = STAGING;
--- src/apis/technologies.js
+++ src/apis/technologies.js
@@ -1,18 +1,19 @@
-import axios from 'axios';
-import { ENV } from './constants';
+import axios from "axios";
 
 export function fetchTechnologies() {
-    return axios.get(`${ENV}/technologies`)
-        .then(function (response) {
-            const { technologies } = response.data;
-            return technologies;
-        });
+  return axios
+    .get(`${process.env.REACT_APP_ENV}/technologies`)
+    .then(function(response) {
+      const { technologies } = response.data;
+      return technologies;
+    });
 }
 
 export function fetchYearsExperience() {
-    return axios.get(`${ENV}/years_experience`)
-        .then(function (response) {
-            const { years_experience } = response.data;
-            return years_experience;
-        });
+  return axios
+    .get(`${process.env.REACT_APP_ENV}/years_experience`)
+    .then(function(response) {
+      const { years_experience } = response.data;
+      return years_experience;
+    });
 }
